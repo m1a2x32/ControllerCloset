a71ae8a74c4f2cda7de734248fdd4cd1af876989
Marco <marcocardillo1234@gmail.com>
2024-10-01 18:48:50 +0200

    GPIO refactored

--- drivers/HAL/GPIO/gpio.cpp
+++ drivers/HAL/GPIO/gpio.cpp
@@ -1,6 +1,4 @@
-#include "gpio_input.h"
-#include "gpio_output.h"
-#include "gpio_af.h"
+#include "gpio_base.h"
 
 namespace HAL::GPIO {
 
@@ -44,64 +42,66 @@ namespace HAL::GPIO {
                 return nullptr;
         }
     }
-}
 
-namespace HAL::GPIO::INPUT {
-    GPIO_INPUT::GPIO_INPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin): IO(_ioPort, _ioPin){
-        set_io_type(GPIO_MODES::INPUT_MODE);
-    }
-    
-    bool GPIO_INPUT::read_state(){
-        return (port->IDR & (1 << pinNr)) != 0;
-    }
-}
+    namespace INPUT {
 
-namespace HAL::GPIO::OUTPUT{
-    GPIO_OUTPUT::GPIO_OUTPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin) : IO(_ioPort, _ioPin){
-        set_io_type(GPIO_MODES::OUTPUT_MODE);
-    }
-
-    void GPIO_OUTPUT::set_pin(){
-        // Todo: double check this work, if not bsrr reg
-        SET_BIT(port->ODR, (1 << pinNr));
-    }
-
-    void GPIO_OUTPUT::reset_pin(){
-        CLEAR_BIT(port->ODR, (1 << pinNr));
-    }
-
-    void GPIO_OUTPUT::set_output_type(TYPE_REG oTypeReg){
-        CLEAR_BIT(port->OTYPER, (oTypeReg << pinNr));
-        if (oTypeReg == PUSH_PULL) return;
-
-        SET_BIT(port->OTYPER, (oTypeReg << pinNr));
-    }
-
-    void GPIO_OUTPUT::set_output_speed(SPEED_REG oSpeedReg){
-        CLEAR_BIT(port->OSPEEDR, (oSpeedReg << (pinNr << 1)));
-        if(oSpeedReg == VERY_LOW_SPEED) return;
-
-        SET_BIT(port->OSPEEDR, (oSpeedReg << (pinNr << 1)));
-    }
-}
-
-namespace HAL::GPIO::AF{
-    GPIO_AF::GPIO_AF(AVAILABLE_PORTS _ioPort, uint8_t _ioPin) : IO(_ioPort, _ioPin){
-        set_io_type(GPIO_MODES::AF_MODE);
-    }
-
-    void GPIO_AF::set_alternate_function(ALTERNTAE_FUNCTIONS nr){
-        if (pinNr <= 7){
-            CLEAR_BIT(port->AFR[0], (nr << (pinNr << 2)));
-            if(nr == AF0) return;
-
-            SET_BIT(port->AFR[0], (nr << (pinNr << 2)));
-        } else {
-            CLEAR_BIT(port->AFR[1], (nr << (pinNr << 2)));
-            if(nr == AF0) return;
-
-            SET_BIT(port->AFR[1], (nr << (pinNr << 2)));
+        GPIO_INPUT::GPIO_INPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin): IO(_ioPort, _ioPin){
+            set_io_type(GPIO_MODES::INPUT_MODE);
         }
         
+        bool GPIO_INPUT::read_state(){
+            return (port->IDR & (1 << pinNr)) != 0;
+        }
+    }
+
+    namespace OUTPUT {
+
+        GPIO_OUTPUT::GPIO_OUTPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin) : IO(_ioPort, _ioPin){
+            set_io_type(GPIO_MODES::OUTPUT_MODE);
+        }
+
+        void GPIO_OUTPUT::set_pin(){
+            // Todo: double check this work, if not bsrr reg
+            SET_BIT(port->ODR, (1 << pinNr));
+        }
+
+        void GPIO_OUTPUT::reset_pin(){
+            CLEAR_BIT(port->ODR, (1 << pinNr));
+        }
+
+        void GPIO_OUTPUT::set_output_type(TYPE_REG oTypeReg){
+            CLEAR_BIT(port->OTYPER, (oTypeReg << pinNr));
+            if (oTypeReg == PUSH_PULL) return;
+
+            SET_BIT(port->OTYPER, (oTypeReg << pinNr));
+        }
+
+        void GPIO_OUTPUT::set_output_speed(SPEED_REG oSpeedReg){
+            CLEAR_BIT(port->OSPEEDR, (oSpeedReg << (pinNr << 1)));
+            if(oSpeedReg == VERY_LOW_SPEED) return;
+
+            SET_BIT(port->OSPEEDR, (oSpeedReg << (pinNr << 1)));
+        }
+    }
+
+    namespace AF {
+        GPIO_AF::GPIO_AF(AVAILABLE_PORTS _ioPort, uint8_t _ioPin) : IO(_ioPort, _ioPin){
+            set_io_type(GPIO_MODES::AF_MODE);
+        }
+
+        void GPIO_AF::set_alternate_function(ALTERNTAE_FUNCTIONS nr){
+            if (pinNr <= 7){
+                CLEAR_BIT(port->AFR[0], (nr << (pinNr << 2)));
+                if(nr == AF0) return;
+
+                SET_BIT(port->AFR[0], (nr << (pinNr << 2)));
+            } else {
+                CLEAR_BIT(port->AFR[1], (nr << (pinNr << 2)));
+                if(nr == AF0) return;
+
+                SET_BIT(port->AFR[1], (nr << (pinNr << 2)));
+            }
+            
+        }
     }
 }
--- drivers/HAL/GPIO/gpio_base.h
+++ drivers/HAL/GPIO/gpio_base.h
@@ -40,4 +40,58 @@ namespace HAL::GPIO{
             void set_io_type(GPIO_MODES reg);
             GPIO_TypeDef *get_port(AVAILABLE_PORTS target);
     };
+
+    namespace INPUT{
+
+        class GPIO_INPUT : private IO {
+            public:
+                GPIO_INPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin);
+                bool read_state();
+        };
+    }
+
+    namespace OUTPUT {
+
+        enum TYPE_REG{
+            OPEN_DRAIN      = GPIO_OTYPER_OT0,
+            PUSH_PULL       = ~GPIO_OTYPER_OT0,
+        };
+
+        enum SPEED_REG{
+            VERY_LOW_SPEED  = ~GPIO_OSPEEDR_OSPEED0,
+            LOW_SPEED       = GPIO_OSPEEDR_OSPEED0_0,
+            HIGH_SPEED      = GPIO_OSPEEDR_OSPEED0_1,
+            VERY_HIGH_SPEED = GPIO_OSPEEDR_OSPEED0,
+        };
+
+        class GPIO_OUTPUT : private IO {
+            public:
+                GPIO_OUTPUT(AVAILABLE_PORTS _ioPort, uint8_t _ioPin);
+                void set_pin();
+                void reset_pin();
+                
+                void set_output_type(TYPE_REG oTypeReg);
+                void set_output_speed(SPEED_REG oSpeedReg);
+        };
+    }
+    
+    namespace AF {
+
+        enum ALTERNTAE_FUNCTIONS {
+            AF0 = 0,
+            AF1 = 1,
+            AF2 = 2,
+            AF3 = 3,
+            AF4 = 4,
+            AF5 = 5,
+            AF6 = 6,
+            AF7 = 7
+        };
+
+        class GPIO_AF : private IO {
+            public:
+                GPIO_AF(AVAILABLE_PORTS _ioPort, uint8_t _ioPin);
+                void set_alternate_function(ALTERNTAE_FUNCTIONS nr);
+        };
+    }
 }
