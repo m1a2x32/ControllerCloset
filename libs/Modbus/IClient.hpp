4a22ac914b6b8118ad81155bbd5f714d249b30ef
Mjc <marcocardillo1234@gmail.com>
2025-02-08 03:22:40 -0500

    added boiler plate

--- libs/Modbus/IClient.hpp
+++ libs/Modbus/IClient.hpp
@@ -21,6 +21,15 @@ namespace Modbus {
     };
     #pragma pack(pop)
 
+    enum mbus_err{
+        MBUS_ERR_NONE,                   // No error
+        MBUS_ERR_TIMEOUT,                // Timeout occurred
+        MBUS_ERR_CONNECTION_FAILED,      // Connection issue
+        MBUS_ERR_TRANSMISSION_ERROR,     // Transmission failure
+        MBUS_ERR_INVALID_RESPONSE,       // Malformed or unexpected response
+        MBUS_ERR_CRC_MISMATCH,           // CRC check failed
+    };
+
     class IClient{
         public:
             IClient(uint8_t slave_address);
@@ -28,8 +37,8 @@ namespace Modbus {
             void read_holding_registers(const uint16_t address,const uint16_t reg_nrs, uint16_t &data, uint8_t &data_size);                         // x03
             void write_holding_register(const uint16_t address, const uint16_t data_to_write, uint16_t &data, uint8_t &data_size);                  // x06
         private:
-            uint8_t slave_address;
-            virtual void send_request(request_mbus_t *mbus, uint32_t timeout_ms) = 0;
-            virtual void receive_response(response_mbus_t *mbus, uint32_t timeout_ms) = 0;
+            uint8_t _slave_address;
+            virtual mbus_err send_request(request_mbus_t *mbus, uint32_t timeout_ms) = 0;
+            virtual mbus_err receive_response(response_mbus_t *mbus, uint32_t timeout_ms) = 0;
     };
 };
--- libs/Modbus/SerialModbus.hpp
+++ libs/Modbus/SerialModbus.hpp
@@ -10,7 +10,7 @@ namespace Modbus {
             SerialClient(uint8_t slave_address, HAL::UART::USART *_uart);
         private:
             HAL::UART::USART *uart;
-            void send_request(request_mbus_t *mbus, uint32_t timeout_ms) override;
-            void receive_response(response_mbus_t *mbus, uint32_t timeout_ms) override;
+            mbus_err send_request(request_mbus_t *mbus, uint32_t timeout_ms) override;
+            mbus_err receive_response(response_mbus_t *mbus, uint32_t timeout_ms) override;
     };
 }
